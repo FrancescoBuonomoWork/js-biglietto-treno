19aedb2ed3e733242ba9416091d14d882b720cf9
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-04 16:48:10 +0200

    fix and add ;

--- js/app.js
+++ js/app.js
@@ -1,46 +1,48 @@
 // # Calcolatore prezzo del biglietto del treno
 
 // - chiedere all' utente i chilometri che vuole percorrere
-const km =parseInt(prompt("quanti chilometri vuoi percorrere?"))
-console.log(typeof km)
+const km =parseInt(prompt("quanti chilometri vuoi percorrere?"));
+console.log(typeof km);
 // - chiedere all'utente la sua eta
-const age =parseInt(prompt("quanti anni hai?"))
-console.log(age)
+const age =parseInt(prompt("quanti anni hai?"));
+console.log(age);
 
 // - calcolare il prezzo che dovra pagare in base ai valori precedentemente dichiarati
 //     - stabilire in una variabile il prezzo al km 
 const pricePerKm = 0.21       
 //     - calcolare il prezzo da pagare in base ai km
 //         - prezzo base = numero km * prezzo per ogni km
-let userPricePerKm = km * pricePerKm
-console.log(userPricePerKm)
+let totalPrice = km * pricePerKm;
+console.log(totalPrice);
 //     - stabilire in delle variabili gli sconti
 //         - calcolare lo sconto da applicare
 //             - sconto = prezzo base * entità sconto / 100
-let discount;
+let discount = 0;
 
 //         - applicare lo sconto determindo il prezzo finale
 //             - prezzo scontato = prezzo base - sconto
 let userDiscountedPrice;
 
 if (age < 18) {
-    discount = userPricePerKm * 20 / 100
+    discount = totalPrice * 20 / 100
     console.log(discount)
-    // userDiscountedPrice = userPricePerKm - discount
+    // userDiscountedPrice = totalPrice - discount
     // console.log(userDiscountedPrice)
+    totalPrice = totalPrice - discount
     
 } else if (age > 65) {
     
-    discount = userPricePerKm * 40 / 100
+    discount = totalPrice * 40 / 100
     console.log(discount)
-} else {
-    discount = 0
+    totalPrice = totalPrice - discount
 }
-userDiscountedPrice = (userPricePerKm - discount)
-console.log(userDiscountedPrice)
-let userDiscountedPriceDecimal = Math.round(userDiscountedPrice * 100) / 100
+
+// userDiscountedPrice = (totalPrice - discount)
+
+console.log(totalPrice.toFixed(2));
+// let userDiscountedPriceDecimal = Math.round(userDiscountedPrice * 100) / 100
 
 
 // - stampare il prezzo che abbiamo calcolato con massimo due decimali  
 const priceDomElement = document.getElementById('price');
-priceDomElement.innerHTML= userDiscountedPriceDecimal
+priceDomElement.innerHTML= totalPrice.toFixed(2);
